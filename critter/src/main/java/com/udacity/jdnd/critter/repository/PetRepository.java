a7c035e304eca1bd0bb68d6d85cff3b15e7cec32
habex <habensf@gmail.com>
2020-12-16 19:52:28 -0800

    PetController CRUD operations with DTO converters add and PetRepository

--- critter/src/main/java/com/udacity/jdnd/critter/repository/PetRepository.java
+++ critter/src/main/java/com/udacity/jdnd/critter/repository/PetRepository.java
@@ -1,11 +1,11 @@
 package com.udacity.jdnd.critter.repository;
 
-import com.udacity.jdnd.critter.data.Customer;
 import com.udacity.jdnd.critter.data.Pet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PetRepository extends JpaRepository<Pet, Long> {
+
     List<Pet> findByCustomerId (long ownerId);
 }
