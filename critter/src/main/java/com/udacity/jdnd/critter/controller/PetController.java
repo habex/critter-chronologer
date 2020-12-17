d06febaed3e25840831b79e72bdab9cc35ce2f08
habex <habensf@gmail.com>
2020-12-16 19:47:28 -0800

    PetController CRUD operations and DTO converters add

--- critter/src/main/java/com/udacity/jdnd/critter/controller/PetController.java
+++ critter/src/main/java/com/udacity/jdnd/critter/controller/PetController.java
@@ -1,7 +1,9 @@
 package com.udacity.jdnd.critter.controller;
 
+import com.udacity.jdnd.critter.data.Customer;
 import com.udacity.jdnd.critter.data.Pet;
 import com.udacity.jdnd.critter.dto.PetDTO;
+import com.udacity.jdnd.critter.service.CustomerService;
 import com.udacity.jdnd.critter.service.PetService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,40 +22,69 @@ public class PetController {
     @Autowired
     PetService petService;
 
+    @Autowired
+    CustomerService customerService;
+
     @PostMapping
     public PetDTO savePet(@RequestBody PetDTO petDTO) {
-        Pet pet = convertPetDTOToPet(petDTO);
+        Pet pet = convertPetDTOToPet(petDTO,new Pet());
         petService.save(pet);
-        return convertPetToPetDTO(pet);
+        //convert pet to petDTO and return
+        return convertPetToPetDTO(pet,new PetDTO());
+    }
+
+    @PutMapping("/{petId}")
+    public void updatePet(@RequestBody PetDTO petDTO, @PathVariable long petId) {
+        Pet pet = petService.findById(petId);
+        convertPetDTOToPet(petDTO,pet);
+        petService.save(pet);
     }
 
     @GetMapping("/{petId}")
     public PetDTO getPet(@PathVariable long petId) {
         Pet pet = petService.findById(petId);
-        return convertPetToPetDTO(pet);
+        return convertPetToPetDTO(pet,new PetDTO());
     }
 
     @GetMapping
-    public List<PetDTO> getPets(){
+    public List<PetDTO> getPets() {
         List<PetDTO> petDTOS = new ArrayList<>();
-        petService.findAll().forEach(pet -> petDTOS.add(convertPetToPetDTO(pet)));
-        return petDTOS ;
+        petService.findAll().forEach(pet -> petDTOS.add(convertPetToPetDTO(pet, new PetDTO())));
+        return petDTOS;
     }
 
     @GetMapping("/owner/{ownerId}")
     public List<PetDTO> getPetsByOwner(@PathVariable long ownerId) {
-        throw new UnsupportedOperationException();
+        List<PetDTO> petDTOs = new ArrayList<>();
+        petService.findByCustomerId(ownerId).forEach(pet -> petDTOs.add(convertPetToPetDTO(pet,null)));
+        return petDTOs;
     }
 
-    private PetDTO convertPetToPetDTO(Pet pet){
-        PetDTO petDTO = new PetDTO();
-        BeanUtils.copyProperties(pet,petDTO);
+    //<editor-fold desc="DTO converters">
+    private PetDTO convertPetToPetDTO(Pet pet, PetDTO petDTO) {
+        BeanUtils.copyProperties(pet, petDTO);
+        if (pet.getCustomer() != null) {
+            petDTO.setOwnerId(pet.getCustomer().getId());
+        }
         return petDTO;
     }
 
-    private Pet convertPetDTOToPet(PetDTO petDTO){
-        Pet pet = new Pet();
-        BeanUtils.copyProperties(petDTO,pet);
+    private Pet convertPetDTOToPet(PetDTO petDTO, Pet pet) {
+
+        if (pet != null) {
+            //Copy everything except id
+            BeanUtils.copyProperties(petDTO, pet, "id");
+        } else {
+            //Copy everything
+            BeanUtils.copyProperties(petDTO, pet);
+        }
+        //Setting ownerId to PetDTO and customer to Pet
+        if (customerService.findById(petDTO.getOwnerId()) != null) {
+            Customer owner = customerService.findById(petDTO.getOwnerId());
+            pet.setCustomer(owner);
+            owner.getPets().add(pet);
+        }
         return pet;
     }
+    //</editor-fold>
 }
